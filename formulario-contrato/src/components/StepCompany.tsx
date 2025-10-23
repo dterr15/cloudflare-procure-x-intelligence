fc1fe2d2d84cc848b15136a165cde7ce420569b2
dterr15 <daniel.terreros9@gmail.com>
2025-10-23 17:16:10 -0300

    Update StepCompany.tsx

--- formulario-contrato/src/components/StepCompany.tsx
+++ formulario-contrato/src/components/StepCompany.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Input } from "./Input";
 import type { CompanyInfo } from "@/types";
 import { formatRut } from "@/utils/security";
@@ -118,4 +117,5 @@ export function StepCompany({ data, onChange, errors }: StepCompanyProps) {
       </div>
     </div>
   );
+
 }
