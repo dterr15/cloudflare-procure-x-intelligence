baf02899daa6ee00d1e49c2b24037b5112231956
dterr15 <daniel.terreros9@gmail.com>
2025-11-04 09:55:42 -0300

    Update main.tsx

--- formulario-contrato/src/main.tsx
+++ formulario-contrato/src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./modal-styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
 );
